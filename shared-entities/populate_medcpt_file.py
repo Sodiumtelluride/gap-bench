0e7e477c7db55341865cfb2d07e131f0ed26b6bf
Sodiumtelluride <gelfandnat@gmail.com>
2026-08-11 11:10:15 -0400

    added shared entities

---
+++ shared-entities/populate_medcpt_file.py
@@ -0,0 +1,31 @@
+import json
+
+# enter the medcpt file you want to be modified here 
+YOUR_MEDCPT_FILE = ""
+# enter the medcpt_results_gold_standard_judge_shared_entities.json file here
+SHARED_ENTITIES_FILE = ""
+
+with open(YOUR_MEDCPT_FILE, "r") as f:
+    medcpt_results = json.load(f)
+
+with open(SHARED_ENTITIES_FILE, "r") as f:
+    shared_entities = json.load(f)
+
+out = []
+for i, rec in enumerate(medcpt_results):
+    if i<100:
+        rec["entities"] = shared_entities[i]["entities"]
+    rec["entities_knn"]= shared_entities[i]["entities_knn"]
+    rec["entities_reranked"]= shared_entities[i]["entities_reranked"]
+    for j, result in enumerate(rec["combined_results"]):
+        result["exact_shared_entities"] = shared_entities[i]["combined_results"][j]["exact_shared_entities"]
+        result["knn_shared_entities"] = shared_entities[i]["combined_results"][j]["knn_shared_entities"]
+    out.append(rec)
+
+# enter the name of your output file here
+with open("results.json", "w") as f:
+    json.dump(out, f, indent=2  )
+
+
+
+
